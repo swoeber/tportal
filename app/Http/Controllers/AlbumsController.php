a553b27f49ac7d8e31d4b3d0f2fa5b065c6043aa
Scott Woeber <swoeber@omegafi.com>
2018-05-25 17:29:35 -0400

    Updated Image
    TODO : Add Databsae

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -12,7 +12,7 @@ class AlbumsController extends BaseController
         array_push($array, [
             'title' => "Prequelle",
             "artist" => "Ghost B.C.",
-            "thumbnail_image" => "https://www.pinterest.com/pin/602215781396009034/",
+            "thumbnail_image" => "https://i.iheart.com/v3/catalog/artist/30001999?ops=fit(480%2C480)%2Crun(%22circle%22)",
             "image" => "https://images-na.ssl-images-amazon.com/images/I/61JCD3p1HPL._SS500.jpg",
             "url" => "https://www.amazon.com/gp/product/B07BZ8VPK6/ref=dm_ws_sp_ps_dp"
         ]);
@@ -20,7 +20,7 @@ class AlbumsController extends BaseController
         array_push($array, [
             'title' => "Ceremony And Devotion",
             "artist" => "Ghost B.C.",
-            "thumbnail_image" => "https://www.pinterest.com/pin/602215781396009034/",
+            "thumbnail_image" => "https://i.iheart.com/v3/catalog/artist/30001999?ops=fit(480%2C480)%2Crun(%22circle%22)",
             "image" => "https://images-na.ssl-images-amazon.com/images/I/61528dyVVHL._SS500.jpg",
             "url" => "https://www.amazon.com/Ceremony-Devotion-Ghost-B-C/dp/B0784Y6RTK/ref=ntt_mus_ep_dpi_3"
         ]);
