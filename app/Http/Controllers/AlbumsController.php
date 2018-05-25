5e38ce591ffed5c7514b63b7b737593e2f3bdc68
Scott Woeber <swoeber@omegafi.com>
2018-05-25 16:44:38 -0400

    Commit to test routes
    Routes for App Albums

---
+++ app/Http/Controllers/AlbumsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class AlbumsController extends BaseController
+{
+    public function index() {
+        $array = [];
+
+        array_push($array, [
+            'title' => "Prequelle",
+            "artist" => "Ghost B.C.",
+            "thumbnail_image" => "https://www.pinterest.com/pin/602215781396009034/",
+            "image" => "https://images-na.ssl-images-amazon.com/images/I/61JCD3p1HPL._SS500.jpg",
+            "url" => "https://www.amazon.com/gp/product/B07BZ8VPK6/ref=dm_ws_sp_ps_dp"
+        ]);
+
+        array_push($array, [
+            'title' => "Ceremony And Devotion",
+            "artist" => "Ghost B.C.",
+            "thumbnail_image" => "https://www.pinterest.com/pin/602215781396009034/",
+            "image" => "https://images-na.ssl-images-amazon.com/images/I/61528dyVVHL._SS500.jpg",
+            "url" => "https://www.amazon.com/Ceremony-Devotion-Ghost-B-C/dp/B0784Y6RTK/ref=ntt_mus_ep_dpi_3"
+        ]);
+
+
+        #title, thumbnail_image, artist, image, url
+
+        return response()->json($array);
+    }
+}
